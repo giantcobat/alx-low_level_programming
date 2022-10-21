7b07604fd7472e8b7ac855170e19402fe98a8d66
giantcobat <prinxtachie@gmail.com>
2022-10-21 00:03:09 +0000

    work

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,16 @@
-#ifndef MAIN_H
+#ifndef MATH_H
 #define MAIN_H
-
 
 #include <stdio.h>
 
+/*The function test if an integer is positive or negative*/
 void positive_or_negative(int i);
-int largest_number(int a, int b, int b);
+
+/*The function that returns the largest of 3 numbers*/
+int largest_number(int a, int b, int c);
+
 int convert_day(int month, int day);
+
 void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
